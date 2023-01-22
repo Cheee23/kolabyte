7381bfebe6b3238461877376ab9f26dc17087a5f
fiqgant <fiqgant@gmail.com>
2023-01-22 17:10:40 +0700

    make it faster using script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,8 +16,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="id">
-        <script><Head>{CssBaseline.flush()}</Head></script>
-        
+        <Head>{CssBaseline.flush()}</Head>
         <script><link href="https://fonts.gstatic.com" rel="preconnect" /></script>
         <script><link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
